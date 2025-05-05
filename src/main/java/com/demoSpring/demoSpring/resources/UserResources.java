cd759c1d0cfd0cc2ae8c30ff19fa7b28eff56a75
Gabriel_Moribe <163215540+gabriel-mrb@users.noreply.github.com>
2025-05-05 19:25:55 -0300

    Implementacao de metodos Insert, Delete, Update

--- src/main/java/com/demoSpring/demoSpring/resources/UserResources.java
+++ src/main/java/com/demoSpring/demoSpring/resources/UserResources.java
@@ -2,13 +2,13 @@ package com.demoSpring.demoSpring.resources;
 
 import com.demoSpring.demoSpring.entidades.User;
 import com.demoSpring.demoSpring.services.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController // INFORMA AO SPRING QUE ESTA CLASSE É UM CONTROLADOR REST
@@ -30,4 +30,21 @@ public class UserResources {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
